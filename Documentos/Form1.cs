b26f707ed33ba2b2af6077bba09b55d24edc89a2
Haim <hgq_2000@hotmail.com>
2021-08-26 11:53:38 -0500

    Seatch Document by ID and Docuemnt number and type

--- Documentos/Form1.cs
+++ Documentos/Form1.cs
@@ -26,6 +26,8 @@ namespace Documentos.Connection
             listProducts.Columns.Add("Clave", -2, HorizontalAlignment.Left);
             listProducts.Columns.Add("Unidad", -2, HorizontalAlignment.Left);
             listProducts.Columns.Add("Producto", -2, HorizontalAlignment.Left);
+            radiobtn_NumDocument.Checked = true;
+            DocumentType.fillDocTypes(comboBoxDocTypes);
             txtDocument.Focus();
 
         }
@@ -33,13 +35,20 @@ namespace Documentos.Connection
         private void txtDocument_KeyPress(object sender, KeyPressEventArgs e) 
         {
             if ((int)e.KeyChar == (int)Keys.Enter && txtDocument.Text != "") {
+                SQLFunction function = new SQLFunction();
                 string documentID = txtDocument.Text.ToString().Trim();
                 try
                 {
-                    SQLFunction function = new SQLFunction();
-                    function.setDocumentAttributes(documentID);
+                    if (radiobtn_NumDocument.Checked) {
+                        int typeID = DocumentType.getDocumentTypeID(comboBoxDocTypes.SelectedItem.ToString());
+                        function.getDocumetByDocumentNumber(documentID,typeID);
+                    }
+                    else if (radioBtn_documentBarcode.Checked) {
+                        function.searchDocumentByBarcode(documentID);
+                    }
                     printDocumentInformation();
                     printProducts();
+                    disableRadioButtons();
                 }
                 catch (SqlException err)
                 {
@@ -48,32 +57,28 @@ namespace Documentos.Connection
                 catch (Exception errorGenerado) {
                     MessageBox.Show(errorGenerado.Message.ToString(), "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
                 }
-                
             }
         }
         private void txtBarcode_KeyPress(object sender, KeyPressEventArgs e) {
             if ((int)e.KeyChar == (int)Keys.Enter) {
-                searchProduct();
+                searchProduct(txtBarcode.Text);
             }
         }
 
-        private void searchProduct() {
-            string barcode = txtBarcode.Text;
+        private void searchProduct(string barcode)
+        {
             txtBarcode.Text = "";
-            Document doc = Document.getInstance();
-            int i = 0;
-            bool productFound = false;
-            foreach (Product producto in doc.getListProducts()) {
-                if (producto.getBarcode() == barcode || producto.getClave() == barcode)
-                {
-                    listProducts.Items[i].Checked = true;
-                    productFound = true;
-                }
-                i++;
-            }
-            if (!productFound) MessageBox.Show("Producto no encontrado en el documento","Alerta",MessageBoxButtons.OK,MessageBoxIcon.Exclamation,MessageBoxDefaultButton.Button1);
-            
+            int indexProduct = Document.getInstance().getIndexIsProductExists(barcode);
+            if(indexProduct != -1)
+                focusProductInListView(indexProduct);
+            else MessageBox.Show("Producto no encontrado en el documento","Alerta",MessageBoxButtons.OK,MessageBoxIcon.Exclamation,MessageBoxDefaultButton.Button1);
+        }
 
+        private void focusProductInListView(int indexProduct) {
+            listProducts.Focus();
+            listProducts.Items[indexProduct].Selected = true;
+            listProducts.Items[indexProduct].Focused = true;
+            listProducts.EnsureVisible(indexProduct);
         }
 
         private void printDocumentInformation(){
@@ -95,20 +100,53 @@ namespace Documentos.Connection
             }
         }
 
-        private void productsDataGrid_CurrentCellChanged(object sender, EventArgs e)
-        {
-            
-        }
-
         private void btnCleanFields_Click(object sender, EventArgs e)
         {
             listProducts.Items.Clear();
+            enableRadioButtons();
             documentTypeLabel.Text = "";
             clientNameLabel.Text = "";
             razonSocialLabel.Text = "";
             txtDocument.Text = "";
             txtDocument.Focus();
             Document.destroy();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e) {
+            if (tabControl1.SelectedIndex == 1)
+                txtBarcode.Focus();
+        }
+
+        private void radiobtn_NumDocument_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radiobtn_NumDocument.Checked) {
+                label1.Text = "Número Documento:";
+                comboBoxDocTypes.Visible = true;
+                labelTipo.Visible = true;
+                txtDocument.Focus();
+            }
+        }
+
+        private void disableRadioButtons() {
+            radiobtn_NumDocument.Enabled = false;
+            radioBtn_documentBarcode.Enabled = false;
+        }
+
+        private void enableRadioButtons()
+        {
+            radiobtn_NumDocument.Enabled = true;
+            radioBtn_documentBarcode.Enabled = true;
+        }
+
+        private void radioBtn_documentBarcode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioBtn_documentBarcode.Checked)
+            {
+                label1.Text = "Código:";
+                comboBoxDocTypes.Visible = false;
+                labelTipo.Visible = false;
+                txtDocument.Focus();
+            }
         }
 
     }
